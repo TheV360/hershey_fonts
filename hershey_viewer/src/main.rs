98e4d751ef75baa44de977c50118bc53c6884712
V360 <v360code@gmail.com>
2024-02-08 03:05:25 -0500

    switch to not minifb
    very quick and dirty port

--- hershey_viewer/src/main.rs
+++ hershey_viewer/src/main.rs
@@ -1,16 +1,23 @@
 use std::fs::{read_dir, read_to_string};
 use std::ffi::OsStr;
+use std::rc::Rc;
+use std::num::NonZeroU32;
 
-use minifb::{Scale, Window, WindowOptions, Key};
+use winit::dpi::LogicalSize;
+// use minifb::{Scale, Window, WindowOptions, Key};
+use winit::event::{Event, KeyEvent, WindowEvent, MouseButton, MouseScrollDelta, ElementState};
+use winit::event_loop::{ControlFlow, EventLoop};
+use winit::keyboard::{Key, NamedKey};
+use winit::window::WindowBuilder;
 // use nanorand::WyRand;
 
 use hershey_reader::*;
 
-const WIDTH: usize = 1280;
-const HEIGHT: usize = 720;
-
 mod bresenham;
 use bresenham::*;
+
+const WIDTH: usize = 1280;
+const HEIGHT: usize = 720;
 
 const CORNER: Vec2 = (WIDTH as Coord, HEIGHT as Coord);
 const CENTER: Vec2 = (CORNER.0 / 2, CORNER.1 / 2);
@@ -71,66 +78,29 @@ fn main() -> std::io::Result<()> {
 	let mut buffer: Box<[u32]> = vec![0x201d1a; WIDTH * HEIGHT]
 		.into_boxed_slice();
 	
-	let mut win = Window::new(
-		"Hershey Font Viewer by V 360", WIDTH, HEIGHT,
-		WindowOptions {
-			scale: Scale::X1,
-			..Default::default()
-		}
-	).expect("Failed to open window!");
+	let event_loop = EventLoop::new().unwrap();
+	let window = Rc::new(
+		WindowBuilder::new()
+			.with_title("Hershey Font Viewer by V 360")
+			.with_inner_size(LogicalSize::new(WIDTH as f32, HEIGHT as f32))
+			.build(&event_loop)
+			.expect("Failed to open window!")
+	);
 	
-	let mut specimen = r"
-the quick brown fox jumped
-over the lazy dog
-THE QUICK BROWN FOX JUMPED
-OVER THE LAZY DOG
-(0123456789)
-<HTML> ? @
-	".trim().to_string();
-	
-	// hardcoded US A layout. sorry.
-	fn key_to_char(k: Key, sh: bool) -> Option<char> {
-		const NM_UP: &[u8] = b")!@#$%^&*(";
-		let kc = k as u8;
-		match (sh, k, kc) {
-			(false, _,  0..= 9) => Some((b'0' +  kc      ) as char),
-			(false, _, 10..=35) => Some((b'a' + (kc - 10)) as char),
-			( true, _,  0..= 9) => Some(NM_UP[kc as usize] as char),
-			( true, _, 10..=35) => Some((b'A' + (kc - 10)) as char),
-			
-			(_, Key::Space, _) => Some(' '),
-			
-			(false, Key::Apostrophe, _) => Some('\''),
-			( true, Key::Apostrophe, _) => Some('"'),
-			(false, Key::Backquote, _) => Some('`'),
-			( true, Key::Backquote, _) => Some('~'),
-			(false, Key::Backslash, _) => Some('\\'),
-			( true, Key::Backslash, _) => Some('|'),
-			(false, Key::Comma, _) => Some(','),
-			( true, Key::Comma, _) => Some('<'),
-			(false, Key::Equal, _) => Some('='),
-			( true, Key::Equal, _) => Some('+'),
-			(false, Key::LeftBracket, _) => Some('['),
-			( true, Key::LeftBracket, _) => Some('{'),
-			(false, Key::Minus, _) => Some('-'),
-			( true, Key::Minus, _) => Some('_'),
-			(false, Key::Period, _) => Some('.'),
-			( true, Key::Period, _) => Some('>'),
-			(false, Key::RightBracket, _) => Some(']'),
-			( true, Key::RightBracket, _) => Some('}'),
-			(false, Key::Semicolon, _) => Some(';'),
-			( true, Key::Semicolon, _) => Some(':'),
-			(false, Key::Slash, _) => Some('/'),
-			( true, Key::Slash, _) => Some('?'),
-			
-			(_, Key::Backspace, _) => Some('\x08'),
-			(_, Key::Delete, _) => Some('\x7F'),
-			(_, Key::Enter, _) => Some('\n'),
-			
-			_ => None
-		}
+	#[cfg(target_arch = "wasm32")]
+	{
+		use winit::platform::web::WindowExtWebSys;
+		
+		web_sys::window().unwrap()
+			.document().unwrap().body().unwrap()
+			.append_child(&window.canvas().unwrap())
+			.unwrap();
 	}
 	
+	let context = softbuffer::Context::new(window.clone()).unwrap();
+	let mut surface = softbuffer::Surface::new(&context, window.clone()).unwrap();
+	
+	#[derive(Clone, Copy)]
 	enum Page {
 		Help,
 		Specimen,
@@ -163,10 +133,11 @@ OVER THE LAZY DOG
 		}
 	}
 	
-	let ui_font = &fonts.iter()
-		.find(|(name, _)| name == "futural.jhf")
-		.unwrap_or_else(|| fonts.first().unwrap())
-		.1;
+	let ui_font = fonts.iter()
+		.enumerate()
+		.find(|(_, (name, _))| name == "futural.jhf")
+		.map(|it| it.0)
+		.unwrap_or(0);
 	
 	let mut specimen = r"
 the quick brown fox jumped
@@ -182,143 +153,210 @@ OVER THE LAZY DOG
 	let mut cur_font = 0;
 	let mut cur_page = Page::Help;
 	let mut font_size = 1.0;
-	let mut redraw = true; // hastily added
 	
 	// gonna use this for gfx buttons
 	let mut mouse = (0, 0);
-	let (mut mouse_click, mut mouse_down) = (false, false);
-	let mut mouse_scroll;
+	let mut mouse_click = false;
+	let mut mouse_scroll = (0, 0);
 	
 	// I did not go into this intending to make this code good. Sorry
 	
-	while win.is_open() {
-		if let Some((x, y)) = win.get_mouse_pos(minifb::MouseMode::Discard) {
-			mouse = (x as Coord, y as Coord);
-		}
-		if let Some((x, y)) = win.get_scroll_wheel() {
-			mouse_scroll = (x as Coord, y as Coord);
-		} else {
-			mouse_scroll = (0, 0);
-		}
+	const GRID_CELLS: Vec2 = (8, 7);
+	
+	const CHRMAP_TL: Vec2 = (CENTER.0, 32);
+	const CHRMAP_BR: Vec2 = (CORNER.0 - 1, CORNER.1 - 1);
+	
+	const GRID_CELLS_F: (f64, f64) = (GRID_CELLS.0 as f64, GRID_CELLS.1 as f64);
+	const CHRMAP_TL_F: (f64, f64) = (CHRMAP_TL.0 as f64, CHRMAP_TL.1 as f64);
+	const CHRMAP_BR_F: (f64, f64) = (CHRMAP_BR.0 as f64, CHRMAP_BR.1 as f64);
+	
+	event_loop.run(move |event, elwt| {
+		elwt.set_control_flow(ControlFlow::Wait);
 		
-		if win.get_mouse_down(minifb::MouseButton::Left) {
-			if !mouse_down && !mouse_click {
-				mouse_click = true;
-				mouse_down = false;
-			} else {
-				mouse_click = false;
-				mouse_down = true;
-			}
-		} else {
-			mouse_click = false;
-			mouse_down = false;
-		}
-		
-		let shift = win.is_key_down(Key::LeftShift) || win.is_key_down(Key::RightShift);
-		if let Some(keys) = win.get_keys_pressed(minifb::KeyRepeat::Yes) {
-			for k in keys {
-				if let Some(ch) = key_to_char(k, shift) {
-					if ch == '\x08' {
-						specimen.pop();
-					} else {
-						specimen.push(ch);
-					}
-					redraw = true;
-				}
-			}
-		}
-		
-		// Switch fonts
-		if cur_font > 0 && win.is_key_pressed(Key::Up, minifb::KeyRepeat::Yes) {
-			cur_font -= 1; redraw = true;
-		}
-		if cur_font < fonts.len() - 1 && win.is_key_pressed(Key::Down, minifb::KeyRepeat::Yes) {
-			cur_font += 1; redraw = true;
-		}
-		let font = &fonts[cur_font];
-		
-		// Switch pages
-		if win.is_key_pressed(Key::PageUp, minifb::KeyRepeat::Yes) {
-			cur_page = cur_page.back(); redraw = true;
-		}
-		if win.is_key_pressed(Key::PageDown, minifb::KeyRepeat::Yes) {
-			cur_page = cur_page.next(); redraw = true;
-		}
-		
-		const GRID_CELLS: Vec2 = (8, 7);
-		
-		const CHRMAP_TL: Vec2 = (CENTER.0, 32);
-		const CHRMAP_BR: Vec2 = (CORNER.0 - 1, CORNER.1 - 1);
-		
-		const GRID_CELLS_F: (f64, f64) = (GRID_CELLS.0 as f64, GRID_CELLS.1 as f64);
-		const CHRMAP_TL_F: (f64, f64) = (CHRMAP_TL.0 as f64, CHRMAP_TL.1 as f64);
-		const CHRMAP_BR_F: (f64, f64) = (CHRMAP_BR.0 as f64, CHRMAP_BR.1 as f64);
-		
-		// Update loop
-		match cur_page {
-			Page::Help => {},
-			Page::Specimen => {
-				if font_size > 0.51 && win.is_key_pressed(Key::NumPadMinus, minifb::KeyRepeat::Yes) {
-					font_size -= 0.25; redraw = true;
-				}
-				if font_size < 6.99 && win.is_key_pressed(Key::NumPadPlus, minifb::KeyRepeat::Yes) {
-					font_size += 0.25; redraw = true;
+		match event {
+			Event::WindowEvent { window_id, .. }
+				if window.id() != window_id
+				=> {}, // sink
+			
+			Event::WindowEvent {
+				event: WindowEvent::CloseRequested, ..
+			} => { elwt.exit(); },
+			
+			Event::WindowEvent {
+				event: WindowEvent::CursorMoved {
+					position, ..
+				}, ..
+			} => { mouse = position.into(); },
+			
+			Event::WindowEvent {
+				event: WindowEvent::MouseWheel {
+					delta, ..
+				}, ..
+			} => { match delta {
+				MouseScrollDelta::LineDelta(x, y) => {
+					let sensitivity = 1.5;
+					let x = (x * sensitivity).floor() as i32;
+					let y = (y * sensitivity).floor() as i32;
+					mouse_scroll = (x, y);
+				},
+				_ => {}
+			}; },
+			
+			Event::WindowEvent {
+				event: WindowEvent::MouseInput {
+					button: MouseButton::Left,
+					state, ..
+				}, ..
+			} => {
+				mouse_click = state == ElementState::Pressed;
+			},
+			
+			Event::WindowEvent { event: WindowEvent::KeyboardInput {
+				event: KeyEvent {
+					logical_key: Key::Named(NamedKey::Backspace),
+					state: ElementState::Pressed, ..
+				}, ..
+			}, .. } => { specimen.pop(); },
+			Event::WindowEvent { event: WindowEvent::KeyboardInput {
+				event: KeyEvent {
+					logical_key: Key::Named(NamedKey::Enter),
+					state: ElementState::Pressed, ..
+				}, ..
+			}, .. } => { specimen.push('\n'); },
+			Event::WindowEvent { event: WindowEvent::KeyboardInput {
+				event: KeyEvent {
+					text: Some(text),
+					state: ElementState::Pressed, ..
+				}, ..
+			}, .. } => { specimen.push_str(&text); },
+			
+			Event::WindowEvent { event: WindowEvent::KeyboardInput {
+				event: KeyEvent {
+					logical_key: Key::Named(
+						key @ (
+							NamedKey::ArrowUp |
+							NamedKey::ArrowDown
+						)
+					),
+					state: ElementState::Pressed, ..
+				}, ..
+			}, .. } => {
+				match key {
+					NamedKey::ArrowUp if cur_font > 0 => {
+						cur_font -= 1; window.request_redraw();
+					},
+					NamedKey::ArrowDown if cur_font < fonts.len() - 1 => {
+						cur_font += 1; window.request_redraw();
+					},
+					_ => {}
+				};
+			},
+			
+			Event::WindowEvent { event: WindowEvent::KeyboardInput {
+				event: KeyEvent {
+					logical_key: Key::Named(
+						key @ (
+							NamedKey::PageUp |
+							NamedKey::PageDown
+						)
+					),
+					state: ElementState::Pressed, ..
+				}, ..
+			}, .. } => {
+				match key {
+					NamedKey::PageUp => {
+						cur_page = cur_page.back();
+						window.request_redraw();
+					},
+					NamedKey::PageDown => {
+						cur_page = cur_page.next();
+						window.request_redraw();
+					},
+					_ => {}
 				}
 			},
-			Page::Map => {
-				let b4_char = cur_char;
-				
-				if cur_char > 0 && win.is_key_pressed(Key::Left, minifb::KeyRepeat::Yes) {
-					cur_char -= 1;
-				}
-				if win.is_key_pressed(Key::Right, minifb::KeyRepeat::Yes) {
-					cur_char += 1;
-				}
-				
-				let chrmap_camera_max = 0.max((font.1.len() as Coord - 1) / GRID_CELLS.0 + 1 - GRID_CELLS.1);
-				
-				if vec2_within_bounds(mouse, CHRMAP_TL, CHRMAP_BR) {
-					if mouse_click {
-						let mouse = invlerp_vec(CHRMAP_TL_F, CHRMAP_BR_F, (mouse.0 as f64, mouse.1 as f64));
-						let mouse = ((mouse.0 * GRID_CELLS_F.0).floor() as Coord, (mouse.1 * GRID_CELLS_F.1).floor() as Coord);
-						cur_char = (mouse.0 + (mouse.1 + chrmap_view) * GRID_CELLS.0) as usize;
+			
+			Event::WindowEvent { event: WindowEvent::KeyboardInput {
+				event: KeyEvent {
+					logical_key: Key::Named(key),
+					state: ElementState::Pressed, ..
+				},
+			.. }, .. } => {
+				// Update loop
+				match cur_page {
+					Page::Help => {},
+					Page::Specimen => {
+						match key {
+							NamedKey::Home if font_size > 0.51 => {
+								font_size -= 0.25; window.request_redraw();
+							},
+							NamedKey::End if font_size < 6.99 => {
+								font_size += 0.25; window.request_redraw();
+							},
+							_ => {}
+						}
+					},
+					Page::Map => {
+						let b4_char = cur_char;
+						let font = &fonts[cur_font];
+						
+						match key {
+							NamedKey::ArrowLeft if cur_char > 0 => { cur_char -= 1; },
+							NamedKey::ArrowRight => { cur_char += 1; },
+							_ => {}
+						}
+						
+						let chrmap_camera_max = 0.max((font.1.len() as Coord - 1) / GRID_CELLS.0 + 1 - GRID_CELLS.1);
+						
+						if vec2_within_bounds(mouse, CHRMAP_TL, CHRMAP_BR) {
+							if mouse_click {
+								let mouse = invlerp_vec(CHRMAP_TL_F, CHRMAP_BR_F, (mouse.0 as f64, mouse.1 as f64));
+								let mouse = ((mouse.0 * GRID_CELLS_F.0).floor() as Coord, (mouse.1 * GRID_CELLS_F.1).floor() as Coord);
+								cur_char = (mouse.0 + (mouse.1 + chrmap_view) * GRID_CELLS.0) as usize;
+								cur_char = 0.max(cur_char.min(font.1.len() - 1));
+							}
+							if mouse_scroll != (0, 0) {
+								let dir = mouse_scroll.1.signum();
+								chrmap_view -= dir;
+								window.request_redraw();
+							}
+						}
+						
 						cur_char = 0.max(cur_char.min(font.1.len() - 1));
-					}
-					if mouse_scroll != (0, 0) {
-						let dir = mouse_scroll.1.signum();
-						chrmap_view -= dir;
-						redraw = true;
-					}
+						
+						if b4_char != cur_char {
+							let cur_row = cur_char as Coord / GRID_CELLS.0;
+							if chrmap_view >= cur_row {
+								chrmap_view -= 1;
+							}
+							if chrmap_view + GRID_CELLS.1 - 1 <= cur_row {
+								chrmap_view += 1;
+							}
+							
+							window.request_redraw();
+						}
+						
+						chrmap_view = 0.max(chrmap_view.min(chrmap_camera_max));
+					},
 				}
-				
-				cur_char = 0.max(cur_char.min(font.1.len() - 1));
-				
-				if b4_char != cur_char {
-					let cur_row = cur_char as Coord / GRID_CELLS.0;
-					if chrmap_view >= cur_row {
-						chrmap_view -= 1;
-					}
-					if chrmap_view + GRID_CELLS.1 - 1 <= cur_row {
-						chrmap_view += 1;
-					}
-					
-					redraw = true;
-				}
-				
-				chrmap_view = 0.max(chrmap_view.min(chrmap_camera_max));
-			},
-		}
-		
-		if redraw {
-			redraw = false;
+			}
 			
-			// Clear screen.
-			buffer.fill(0x201d1a);
-			
-			let buf = &mut buffer;
-			match cur_page {
-				Page::Help => {
-					const HELP_TEXT: &str = r"
+			Event::WindowEvent { event: WindowEvent::RedrawRequested, .. } => {
+				surface.resize(
+					NonZeroU32::new(WIDTH as u32).unwrap(),
+					NonZeroU32::new(HEIGHT as u32).unwrap()
+				).unwrap();
+				
+				// Clear screen.
+				buffer.fill(0x201d1a);
+				
+				let buf = &mut buffer;
+				let font = &fonts[cur_font];
+				let ui_font = &fonts[ui_font].1;
+				
+				match cur_page {
+					Page::Help => {
+						const HELP_TEXT: &str = r"
 Help
 
 Hello! Welcome to my Font Viewer.
@@ -332,70 +370,75 @@ Use Numpad +/- to control size.
 Map:
 Use </>/Click to select.
 Use scroll wheel to scroll.
-					";
-					draw_hershey_str(buf, ui_font, HELP_TEXT, (64, 24), 1.5, COOL_COLORS[0]);
-				},
-				Page::Specimen => {
-					let tooltip = format!("{}\n#{}: {} (x{:.2})", cur_page.get_name(), cur_font, font.0, font_size);
-					draw_hershey_str(buf, ui_font, &tooltip, (32, HEIGHT as Coord - 64), 1.0, COOL_COLORS[0]);
-					
-					let specimen = if specimen.is_empty() { "Type some text..." } else { &specimen };
-					draw_hershey_str(buf, &font.1, specimen, (64, 96), font_size, COOL_COLORS[0]);
-				},
-				Page::Map => {
-					let tooltip = format!("{}\n#{}: {}; {}", cur_page.get_name(), cur_font, font.0, cur_char);
-					draw_hershey_str(buf, ui_font, &tooltip, (32, 40), 0.75, COOL_COLORS[0]);
-					
-					const CHR_SIZE: f64 = 8.0;
-					
-					let chr = &font.1[cur_char];
-					let middle = (CENTER.0 / 2, CENTER.1);
-					draw_hershey_char(buf, chr, middle, CHR_SIZE, COOL_COLORS[0]);
-					
-					for j in 0..GRID_CELLS.1 {
-						for i in 0..GRID_CELLS.0 {
-							let ci = (i + (j + chrmap_view) * GRID_CELLS.0) as usize;
-							
-							// hell
-							let topleft = (i as f64 / GRID_CELLS_F.0, j as f64 / GRID_CELLS_F.1);
-							let center = ((i as f64 + 0.5) / GRID_CELLS_F.0, (j as f64 + 0.5) / GRID_CELLS_F.1);
-							let bottomright = ((i as f64 + 1.0) / GRID_CELLS_F.0, (j as f64 + 1.0) / GRID_CELLS_F.1);
-							
-							let tl = lerp_vec(CHRMAP_TL_F, CHRMAP_BR_F, topleft);
-							let ce = lerp_vec(CHRMAP_TL_F, CHRMAP_BR_F, center);
-							let br = lerp_vec(CHRMAP_TL_F, CHRMAP_BR_F, bottomright);
-							
-							let tl = (tl.0.floor() as Coord, tl.1.floor() as Coord);
-							let br = (br.0.ceil() as Coord - 1, br.1.ceil() as Coord - 1);
-							
-							let cpos = (ce.0.round() as Coord, ce.1.round() as Coord);
-							
-							if let Some(chr) = font.1.get(ci) {
-								let is_current = ci == cur_char;
+						";
+						draw_hershey_str(buf, ui_font, HELP_TEXT, (64, 24), 1.5, COOL_COLORS[0]);
+					},
+					Page::Specimen => {
+						let tooltip = format!("{}\n#{}: {} (x{:.2})", cur_page.get_name(), cur_font, font.0, font_size);
+						draw_hershey_str(buf, ui_font, &tooltip, (32, HEIGHT as Coord - 64), 1.0, COOL_COLORS[0]);
+						
+						let specimen = if specimen.is_empty() { "Type some text..." } else { &specimen };
+						draw_hershey_str(buf, &font.1, specimen, (64, 96), font_size, COOL_COLORS[0]);
+					},
+					Page::Map => {
+						let tooltip = format!("{}\n#{}: {}; {}", cur_page.get_name(), cur_font, font.0, cur_char);
+						draw_hershey_str(buf, ui_font, &tooltip, (32, 40), 0.75, COOL_COLORS[0]);
+						
+						const CHR_SIZE: f64 = 8.0;
+						
+						let chr = &font.1[cur_char];
+						let middle = (CENTER.0 / 2, CENTER.1);
+						draw_hershey_char(buf, chr, middle, CHR_SIZE, COOL_COLORS[0]);
+						
+						for j in 0..GRID_CELLS.1 {
+							for i in 0..GRID_CELLS.0 {
+								let ci = (i + (j + chrmap_view) * GRID_CELLS.0) as usize;
 								
-								draw_rect(buf, tl, br, COOL_COLORS[1]);
-								draw_hershey_char(buf, chr, cpos, if is_current { 1.25 } else { 1.0 }, COOL_COLORS[0]);
+								// hell
+								let topleft = (i as f64 / GRID_CELLS_F.0, j as f64 / GRID_CELLS_F.1);
+								let center = ((i as f64 + 0.5) / GRID_CELLS_F.0, (j as f64 + 0.5) / GRID_CELLS_F.1);
+								let bottomright = ((i as f64 + 1.0) / GRID_CELLS_F.0, (j as f64 + 1.0) / GRID_CELLS_F.1);
 								
-								if is_current {
-									const INSET_AMT: Coord = 3;
-									draw_rect(buf, (tl.0 + INSET_AMT, tl.1 + INSET_AMT), (br.0 - INSET_AMT, br.1 - INSET_AMT), COOL_COLORS[1]);
+								let tl = lerp_vec(CHRMAP_TL_F, CHRMAP_BR_F, topleft);
+								let ce = lerp_vec(CHRMAP_TL_F, CHRMAP_BR_F, center);
+								let br = lerp_vec(CHRMAP_TL_F, CHRMAP_BR_F, bottomright);
+								
+								let tl = (tl.0.floor() as Coord, tl.1.floor() as Coord);
+								let br = (br.0.ceil() as Coord - 1, br.1.ceil() as Coord - 1);
+								
+								let cpos = (ce.0.round() as Coord, ce.1.round() as Coord);
+								
+								if let Some(chr) = font.1.get(ci) {
+									let is_current = ci == cur_char;
+									
+									draw_rect(buf, tl, br, COOL_COLORS[1]);
+									draw_hershey_char(buf, chr, cpos, if is_current { 1.25 } else { 1.0 }, COOL_COLORS[0]);
+									
+									if is_current {
+										const INSET_AMT: Coord = 3;
+										draw_rect(buf, (tl.0 + INSET_AMT, tl.1 + INSET_AMT), (br.0 - INSET_AMT, br.1 - INSET_AMT), COOL_COLORS[1]);
+									}
 								}
 							}
 						}
-					}
-					
-					let bottom = (middle.0, CORNER.1 - 1);
-					let lh = ((chr.left_hand as f64 * CHR_SIZE) as Coord + bottom.0, bottom.1);
-					let rh = ((chr.right_hand as f64 * CHR_SIZE) as Coord + bottom.0, bottom.1);
-					draw_line(buf, lh, rh, COOL_COLORS[1]);
-					draw_line(buf, (lh.0, lh.1 - (HEIGHT as Coord / 16)), lh, COOL_COLORS[1]);
-					draw_line(buf, (rh.0, rh.1 - (HEIGHT as Coord / 16)), rh, COOL_COLORS[1]);
-				},
+						
+						let bottom = (middle.0, CORNER.1 - 1);
+						let lh = ((chr.left_hand as f64 * CHR_SIZE) as Coord + bottom.0, bottom.1);
+						let rh = ((chr.right_hand as f64 * CHR_SIZE) as Coord + bottom.0, bottom.1);
+						draw_line(buf, lh, rh, COOL_COLORS[1]);
+						draw_line(buf, (lh.0, lh.1 - (HEIGHT as Coord / 16)), lh, COOL_COLORS[1]);
+						draw_line(buf, (rh.0, rh.1 - (HEIGHT as Coord / 16)), rh, COOL_COLORS[1]);
+					},
+				}
+				
+				let mut surf_buff = surface.buffer_mut().unwrap();
+				surf_buff.copy_from_slice(&buffer);
+				surf_buff.present().unwrap();
 			}
+			
+			_ => {}
 		}
-		
-		win.update_with_buffer(&buffer, WIDTH, HEIGHT).unwrap();
-	}
+	}).unwrap();
 	
 	Ok(())
 }
